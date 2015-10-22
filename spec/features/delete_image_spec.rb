fde7775e58c47bcf48880a3b7f7fadfbc8071faf
Aileen Cacayorin & Janice Laset Parkerson <student@epicodus.com>
2015-10-22 12:25:15 -0700

    add passing spec to delete image from album

---
+++ spec/features/delete_image_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add image process" do
+
+  it 'adds an image to an album' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    visit '/'
+    click_link "Create Album"
+    fill_in 'Name', with: "Vacation"
+    fill_in 'Description', with: "Best trip ever"
+    click_button "Create Album"
+    click_link "Vacation"
+    attach_file('image_picture', 'spec/support/IMG_4786.jpg')
+    click_button "Add Image"
+    page.find(:css, ".glyphicon.glyphicon-trash").click
+    expect(page).to have_content("Image added")
+  end
+end
